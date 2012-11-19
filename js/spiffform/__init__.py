4924cdae0143135c2f8e6187a0e13da58c03307b
Josip Delic <delijati@googlemail.com>
2012-11-19 13:03:55 +0100

    add: added i18n js to resources

--- js/spiffform/__init__.py
+++ js/spiffform/__init__.py
@@ -4,11 +4,19 @@ import js.jqueryui
 
 library = Library('spiffform', 'resources')
 
+i18n = Resource(library, 'lib/jquery.i18n.min.js',
+                depends=[js.jquery.jquery])
+
 spiffform = Resource(library, 'spiffform/spiffform.js',
-        depends=[js.jquery.jquery, js.jqueryui.jqueryui, js.jqueryui.base])
+                     depends=[js.jquery.jquery, js.jqueryui.jqueryui,
+                              js.jqueryui.base, i18n])
 
 spiffform_default_css = Resource(library, 'default.css',
-        depends=[])
+                                 depends=[])
 
 spiffform_css = Resource(library, 'spiffform/res/spiffform.css',
-        depends=[])
+                         depends=[])
+
+#i18n DE
+spiffform_de = Resource(library, 'spiffform/spiffform-de.js',
+                        depends=[spiffform, i18n])
